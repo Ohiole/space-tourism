460d0cf35183c27d8594846744146e0b95b889ae
Ohiole <eboreimeojo99@gmail.com>
2023-11-02 18:57:52 +0100

    Creating the Header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomeBg from './assets/home/background-home-desktop.jpg';
 function App() {
   return (
     <div className="App relative">
-      <img src={HomeBg} alt='Page Background' className='w-full absolute inset-0 -z-10'/>
+      <img src={HomeBg} alt='Page Background' className='w-full h-screen absolute top-0 -z-10'/>
       <Header />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,16 @@ import Logo from '../assets/shared/logo.svg';
 
 function Header() {
   return (
-    <header>
+    <header className='w-full px-10 py-6 text-white flex justify-between items-center'>
         <img src={Logo} alt="Company Logo" />
+        <nav>
+          <ul>
+            <li>00 <span>HOME</span></li>
+            <li>01 <span>DESTINATION</span></li>
+            <li>02 <span>CREW</span></li>
+            <li>03 <span>TECHNOLOGY</span></li>
+          </ul>
+        </nav>
     </header>
   )
 }
