574c3158f8beb83eb365f38ed0656b7cdb7712c2
Ohiole <eboreimeojo99@gmail.com>
2023-11-03 10:22:57 +0100

    New Configuration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,26 @@ import Logo from '../assets/shared/logo.svg';
 
 function Header() {
   return (
-    <header className='w-full px-10 py-6 text-white flex justify-between items-center'>
+    <header className='w-full pl-10 py-6 flex justify-between items-center'>
         <img src={Logo} alt="Company Logo" />
-        <nav>
-          <ul>
-            <li>00 <span>HOME</span></li>
-            <li>01 <span>DESTINATION</span></li>
-            <li>02 <span>CREW</span></li>
-            <li>03 <span>TECHNOLOGY</span></li>
+        <nav className='basis-3/5 bg-header-bg backdrop-blur-sm flex items-center justify-center'>
+          <ul className='text-white flex gap-20'>
+            <li className='relative flex flex-col justify-center items-center cursor-pointer py-8 group'>
+              <p>00 <span>HOME</span></p>
+              <div className='absolute bottom-0 w-full h-1 bg-white group-hover:bg-link-hover transition-all duration-300 ease-in-out'></div>
+            </li>
+            <li className='relative flex flex-col justify-center items-center cursor-pointer py-8 group'>
+              <p>01 <span>DESTINATION</span></p>
+              <div className='absolute bottom-0 w-full h-1 group-hover:bg-link-hover transition-all duration-300 ease-in-out'></div>
+            </li>
+            <li className='relative flex flex-col justify-center items-center cursor-pointer py-8 group'>
+              <p>02 <span>CREW</span></p>
+              <div className='absolute bottom-0 w-full h-1 group-hover:bg-link-hover transition-all duration-300 ease-in-out'></div>
+            </li>
+            <li className='relative flex flex-col justify-center items-center cursor-pointer py-8 group'>
+              <p>03 <span>TECHNOLOGY</span></p>
+              <div className='absolute bottom-0 w-full h-1 group-hover:bg-link-hover transition-all duration-300 ease-in-out'></div>
+            </li>
           </ul>
         </nav>
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'header-bg': 'rgba(255, 255, 255, 0.1)',
+        'link-hover': 'rgba(255, 255, 255, 0.6)',
+      }
+    },
   },
   plugins: [],
 }
