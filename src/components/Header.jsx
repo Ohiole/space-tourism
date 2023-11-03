1a864ffd66d30a5e7f245fc0d069dbda8817852e
Ohiole <eboreimeojo99@gmail.com>
2023-11-03 10:52:46 +0100

    Shortening the tailwind css code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,23 +5,23 @@ function Header() {
   return (
     <header className='w-full pl-10 py-6 flex justify-between items-center'>
         <img src={Logo} alt="Company Logo" />
-        <nav className='basis-3/5 bg-header-bg backdrop-blur-sm flex items-center justify-center'>
+        <nav className='basis-3/5 bg-header-bg backdrop-blur-sm flex items-center justify-center add-before'>
           <ul className='text-white flex gap-20'>
-            <li className='relative flex flex-col justify-center items-center cursor-pointer py-8 group'>
+            <li>
               <p>00 <span>HOME</span></p>
-              <div className='absolute bottom-0 w-full h-1 bg-white group-hover:bg-link-hover transition-all duration-300 ease-in-out'></div>
+              <div className='line-under active-line'></div>
             </li>
-            <li className='relative flex flex-col justify-center items-center cursor-pointer py-8 group'>
+            <li>
               <p>01 <span>DESTINATION</span></p>
-              <div className='absolute bottom-0 w-full h-1 group-hover:bg-link-hover transition-all duration-300 ease-in-out'></div>
+              <div className='line-under'></div>
             </li>
-            <li className='relative flex flex-col justify-center items-center cursor-pointer py-8 group'>
+            <li>
               <p>02 <span>CREW</span></p>
-              <div className='absolute bottom-0 w-full h-1 group-hover:bg-link-hover transition-all duration-300 ease-in-out'></div>
+              <div className='line-under'></div>
             </li>
-            <li className='relative flex flex-col justify-center items-center cursor-pointer py-8 group'>
+            <li>
               <p>03 <span>TECHNOLOGY</span></p>
-              <div className='absolute bottom-0 w-full h-1 group-hover:bg-link-hover transition-all duration-300 ease-in-out'></div>
+              <div className='line-under'></div>
             </li>
           </ul>
         </nav>
