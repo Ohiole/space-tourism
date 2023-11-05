940cb08a30b97a0ed7c0f93f74bcceb68fce74c2
Ohiole <eboreimeojo99@gmail.com>
2023-11-05 23:43:53 +0100

    Trying to change the basis of the nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Logo from '../assets/shared/logo.svg';
 import { Link } from 'react-router-dom';
+import MenuBar from '../assets/shared/icon-hamburger.svg';
 
 function Header() {
   return (
     <header className='w-full pl-10 py-6 flex justify-between items-center font-sans'>
         <img src={Logo} alt="Company Logo" />
-        <nav className='basis-3/5 bg-header-bg backdrop-blur-sm flex items-center justify-center add-before'>
-          <ul className='text-white flex ml-4 gap-10 lg:gap-20'>
+        <img src={MenuBar} alt="The hamburger and close menu icon" className='md:hidden' />
+        <nav className='basis-3/5  bg-header-bg backdrop-blur-sm flex items-center justify-center add-before absolute md:relative right-0 top-0 h-full'>
+          <ul className='text-white flex flex-col md:flex-row ml-4 gap-10 lg:gap-20'>
             <li>
               <Link to='/'>
                 <p><span className='hidden lg:block'>00</span> <span>HOME</span></p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
   ],
   theme: {
     screens: {
-      'lg': '800px'
+      'lg': '800px',
+      'md': '525px',
     },
     extend: {
       colors: {
