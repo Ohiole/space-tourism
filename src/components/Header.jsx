5394bbbc98eafb32465503f826e372df6c28813a
Ohiole <eboreimeojo99@gmail.com>
2023-11-06 10:55:19 +0100

    Changed the effect on the desktop screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,8 +15,8 @@ function Header() {
     <header className='w-full pl-10 py-6 flex justify-between items-center font-sans'>
         <img src={Logo} alt="Company Logo" />
         <img src={menu ? CloseMenu : MenuBar} alt="The hamburger and close menu icon" className='md:hidden z-40 cursor-pointer mr-4' onClick={toggleMenu}/>
-        <nav className={`w-2/3 md:w-4/5 lg:w-[42rem] xl:w-128 bg-header-bg backdrop-blur-2xl md:backdrop-blur-lg md:flex md:items-center pt-24 md:pt-0 md:justify-center add-before fixed md:relative transition-all duration-500 ease-in-out top-0 h-full ${menu ? 'right-0' : '-right-96'}`}>
-          <ul className='w-full text-white flex flex-col md:flex-row md:ml-4 gap-4 md:gap-10 lg:gap-20'>
+        <nav className={`w-2/3 md:w-4/5 lg:w-[42rem] xl:w-128 bg-header-bg backdrop-blur-2xl md:backdrop-blur-lg md:flex md:items-center pt-24 md:pt-0 md:justify-center add-before fixed md:relative transition-all md:right-0 duration-500 ease-in-out top-0 h-full ${menu ? 'right-0' : '-right-96'}`}>
+          <ul className='w-full text-white flex flex-col md:flex-row md:ml-4 gap-4 md:w-auto md:gap-10'>
             <li>
               <Link to='/'>
                 <p><span className='block md:hidden lg:block'>00</span> <span className='nav'>HOME</span></p>
