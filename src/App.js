e9c5d93a8ed9ac8956ac4e110703f3159681b839
Ohiole <eboreimeojo99@gmail.com>
2023-11-06 12:19:34 +0100

    Finished the mobile and tablet view of home page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomeBg from './assets/home/background-home-desktop.jpg';
+import HomeBgTablet from './assets/home/background-home-tablet.jpg';
+import HomeBgMobile from './assets/home/background-home-mobile.jpg';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App relative h-screen w-full">
-      <img src={HomeBg} alt='Page Background' className='w-full h-screen absolute top-0 -z-10'/>
+      <img src={HomeBg} alt='Desktop Page Background' className='w-full h-screen absolute top-0 -z-10 hidden lg:block'/>
+      <img src={HomeBgTablet} alt='Tablet Page Background' className='w-full h-screen absolute top-0 -z-10 hidden lg:hidden md:block'/>
+      <img src={HomeBgMobile} alt='Desktop Page Background' className='w-full h-screen absolute top-0 -z-10 block md:hidden'/>
       <Header />
       <Routes>
         <Route path='/' Component={Home} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 function Home() {
   return (
-    <main className='flex flex-col justify-end w-full h-3/4'>
-        <section className='flex w-full justify-evenly items-center'>
-            <section className='basis-1/2'>
-                <h3 className='text-new-purple text-2xl tracking-widest'>SO, YOU WANT TO TRAVEL TO</h3>
-                <h1 className='text-9xl text-white mt-8 font-serif'>SPACE</h1>
-                <p className='text-base text-new-purple mt-8 w-96 text-justify font-mono'>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+    <main className='flex flex-col justify-end w-full h-3/4 mt-10 lg:mt-0'>
+        <section className='flex flex-col h-full lg:h-auto lg:flex-row w-full lg:justify-evenly items-center'>
+            <section className='basis-1/2 flex flex-col lg:items-start items-center'>
+                <h3 className='text-new-purple text-xl md:text-2xl tracking-widest'>SO, YOU WANT TO TRAVEL TO</h3>
+                <h1 className='text-7xl md:text-9xl text-white mt-8 font-serif'>SPACE</h1>
+                <p className='text-base text-new-purple mt-8 w-full px-4 md:px-0 md:w-96 text-center lg:text-justify font-mono'>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p> 
             </section>
-            <section className='group'>
-                <section className='h-96 w-96 rounded-full flex items-center justify-center bg-transparent group-hover:bg-header-bg transition-all ease-in-out duration-300'>
-                    <button className='w-64 h-64 rounded-full bg-white font-serif flex items-center justify-center outline-none border-none text-3xl'>EXPLORE</button>
+            <section className='group mt-4 lg:mt-0'>
+                <section className='h-64 w-64 lg:h-96 lg:w-96 rounded-full flex items-center justify-center bg-transparent group-hover:bg-header-bg transition-all ease-in-out duration-300'>
+                    <button className='h-48 w-48 lg:w-64 lg:h-64 rounded-full bg-white font-serif flex items-center justify-center outline-none border-none text-3xl'>EXPLORE</button>
                 </section>
             </section>
         </section>
